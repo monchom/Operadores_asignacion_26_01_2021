40c6c21e77592bee9261402a533df50257d5717e
Adrian velasquez Guerrero <adrian.velasquez.guerrero@gmail.com>
2021-03-08 20:16:43 -0500

    operadores_asignacion

---
+++ main.js
@@ -0,0 +1,68 @@
+
+//Adrian Velasquez Guerrero
+//Operadores de asignación
+let x = 10;
+let y = 20;
+
+//Definición
+x=y;
+//Significado
+x=y;
+console.log("Asignación: "+ x);
+
+//Operadores de Adición
+x = 10;
+y = 20;
+
+//Abreviado
+x+=y;
+//Significado
+x = x+y
+console.log("Adición: "+ x);
+
+//Operadores de sustracción
+x = 10;
+y = 20;
+
+//Abreviado
+x-=y;
+//Significado
+x = x-y;
+
+console.log("Sustracción: "+ x);
+
+//Operadores de multiplicación
+x = 10;
+y = 20;
+//Abreviado
+x*=y;
+//Significado
+x=x*y;
+console.log("Multiplicación: "+ x);
+
+//Operadores de división
+x = 10;
+y = 20;
+//Abreviado
+x/=y;
+//Significado
+x=x/y;
+console.log("División: "+ x);
+
+//Operadores de residuo
+x = 10;
+y = 20;
+//Abreviado
+x%=y;
+//Significado
+x=x%y;
+console.log("Residuo: "+ x);
+
+//Operadores de exponenciación
+a = 10;
+b = 2;
+//Abreviado
+a**=b;
+//Significado
+a=a**b;
+console.log("Exponenciación: "+ a);
